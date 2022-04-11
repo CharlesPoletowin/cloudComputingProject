ca07899d254179c7e235abdad22632c3c9fb856e
CharlesPoleToWin <1409985968@qq.com>
2022-04-11 12:56:02 -0400

    add nav bar

---
+++ src/common/userStatus.js
@@ -0,0 +1,17 @@
+import {useDispatch} from 'react-redux'
+import {login, logout} from '../redux/action'
+import { useEffect } from 'react'
+
+const UserApp = (props) => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(login(props.user))
+    })
+   
+    return (
+        <div></div>
+    )
+}
+
+export default UserApp
