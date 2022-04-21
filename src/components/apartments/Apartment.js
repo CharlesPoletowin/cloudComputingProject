d899f03288407c70acb150fa418507aaebcc1942
CharlesPoleToWin <1409985968@qq.com>
2022-04-21 19:36:31 -0400

    add apartment-base chores

--- src/components/apartments/Apartment.js
+++ src/components/apartments/Apartment.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Apartment.css';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 const api = axios.create({
     baseURL: "https://kgr24rcf4c.execute-api.us-west-2.amazonaws.com/dev"
@@ -30,10 +31,13 @@ function Apartment() {
                 status: "roommates",
             }
         ).then(({data}) => data)
+        console.log(getRoommatesRes)
         if (getRoommatesRes.statusCode === 200) {
             const aptInfo = JSON.parse(getRoommatesRes.body)
-            setApartmentName(aptInfo["apartmentId"])
-            setRoommates(aptInfo["roommates"].join(", "))
+            if (aptInfo["apartmentId"] !== undefined) {
+                setApartmentName(aptInfo["apartmentId"])
+                setRoommates(aptInfo["roommates"].join(", "))
+            }
         }
     }
 
@@ -62,6 +66,7 @@ function Apartment() {
                     apartmentName={apartmentName}
                     addNewRoommate={addNewRoommate}
                 />
+                <Todo apartmentName={apartmentName}/>
             </div>
         )
     }
@@ -165,3 +170,199 @@ function AddRoommate({user, apartmentName, addNewRoommate}) {
 }
 
 export default Apartment
+
+
+const api1 = axios.create({
+    baseURL: "https://ugnn69x209.execute-api.us-east-1.amazonaws.com/dev"
+})
+
+
+const title = "Manage House Chores"
+
+function CreateTask({ addTask, user }) {
+    const [value, setValue] = useState("")
+    const [dateValue, setDate] = useState("2022-03-30")
+    const [timeValue, setTime] = useState("22:00")
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        if (!user | !user.attributes | !user.attributes.email) return;
+
+        var objSubmit = {}
+        objSubmit["userId"] = user.attributes.email
+        objSubmit["status"] = "create"
+        objSubmit["time"] = Date.now().toString()
+        objSubmit["title"] = value
+        objSubmit["deadlineDate"] = dateValue
+        objSubmit["deadlineTime"] = timeValue
+        objSubmit["completed"] = false
+
+        api1.post("/",
+            objSubmit
+        ).then(_res => {
+            // console.log(_res)
+            addTask(objSubmit);
+        }).catch(error => {
+            console.log(error)
+        })
+
+        // addTask(value);
+        setValue("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                placeholder="Add a new task"
+                onChange={e => setValue(e.target.value)}
+                required
+            />
+            <input
+                type="date"
+                className="input"
+                id="date"
+                name="finish-date"
+                value={dateValue}
+                min="2000-01-01"
+                max="2099-12-31"
+                onChange={e => setDate(e.target.value)}
+                required
+            />
+            <input
+                type="time"
+                id="appt"
+                name="appt"
+                value={timeValue}
+                onChange={e => setTime(e.target.value)}
+                required
+            />
+            <input
+                type="submit"
+                value="Submit"
+                style={{
+                    background: "#649cf5",
+                    color: 'white',
+                    fontSize: "20px",
+                    fontWeight: "bold"
+                }}
+            />
+        </form>
+    );
+}
+
+
+function Task({ task, index, completeTask, removeTask }) {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            <div style={{display:"flex", flexDirection:"row", width:"100%"}}>
+                <div style={{display:"flex", flexDirection:"column", width:"100%"}}>
+                    {task.title}
+                    <div style={{fontSize:"8px"}}>deadline: {task.deadlineDate} {task.deadlineTime}</div>
+                </div>
+                <div style={{width:"100%"}}>
+                    <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
+                    <button onClick={() => completeTask(index)}>Complete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
+function Todo({apartmentName}) {
+
+    const user = useSelector(e => e.user)
+
+    const [tasks, setTasks] = useState(
+        () => {
+                return  [
+                ]
+            }
+    );
+
+    const getData = async() => {
+        if (!user | !user.attributes | !user.attributes.email) return;
+        if (!apartmentName) return
+        var res = await api1.get("/",
+            {
+                params: {
+                    userId: apartmentName
+                }
+            }
+        ).then(({data}) => data);
+        setTasks(JSON.parse(res.body))
+    }
+
+
+    const addTask = objSubmit => {
+        const newTasks = [...tasks, objSubmit];
+        setTasks(newTasks);
+    };
+
+    const completeTask = index => {
+        if (!user | !user.attributes | !user.attributes.email) return;
+        if (!apartmentName) return
+        const newTasks = [...tasks];
+        newTasks[index].completed = !newTasks[index].completed;
+
+        var objSubmit = {}
+        objSubmit["userId"] = apartmentName
+        objSubmit["status"] = "update"
+        objSubmit["time"] = newTasks[index].time
+        objSubmit["completed"] = newTasks[index].completed
+        api1.post("/", objSubmit).then(_res => {
+            // console.log(_res)
+            setTasks(newTasks);
+        }).catch(error => {
+            console.log(error)
+        })
+    };
+
+    const removeTask = index => {
+        if (!user | !user.attributes | !user.attributes.email) return;
+        const newTasks = [...tasks];
+        var objSubmit = {}
+        objSubmit["userId"] = apartmentName
+        objSubmit["status"] = "delete"
+        objSubmit["time"] = newTasks[index].time
+        api1.post("/", objSubmit).then(_res => {
+            // console.log(_res)
+            newTasks.splice(index, 1);
+            setTasks(newTasks);
+        }).catch(error => {
+            console.log(error)
+        })
+
+    };
+
+    useEffect(getData, [user])
+
+    return (
+        <div className="todo-container1">
+            <div className="header">{title}</div>
+            <RefreshIcon onClick={getData} />
+
+            <div className="tasks">
+                {tasks.map((task, index) => (
+                    <Task
+                        task={task}
+                        index={index}
+                        completeTask={completeTask}
+                        removeTask={removeTask}
+                        key={index}
+                    />
+                ))}
+            </div>
+            <div className="create-task" >
+                <CreateTask addTask={addTask} user={user} />
+            </div>
+        </div>
+    );
+}
--- src/routes/apartment.js
+++ src/routes/apartment.js
@@ -2,7 +2,7 @@ import Apartment from '../components/apartments/Apartment'
 
 export default function apartment() {
     return (
-        <main style={{ padding: "1rem 0", background: 'rgb(220,220,220)' }}>
+        <main style={{ padding: "0.5rem 0.5rem", background: 'rgb(220,220,220)'}}>
             <h2>Apartment</h2>
             <Apartment/>
         </main>
